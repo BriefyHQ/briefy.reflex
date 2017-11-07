ff29671a2d71d730e32126e1c327fa211dd7e170
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-11-07 21:43:12 +0100

    Remove file after upload in S3.

--- src/briefy/reflex/tasks/s3.py
+++ src/briefy/reflex/tasks/s3.py
@@ -1,11 +1,12 @@
 """Communication with amazon S3 service."""
-from briefy.common.config import ENV
+from briefy.common.config import _queue_suffix
 from briefy.reflex import config
 from briefy.reflex import logger
 from briefy.reflex.celery import app
 from briefy.reflex.tasks import ReflexTask
 
 import boto3
+import os
 import typing as t
 
 
@@ -18,13 +19,10 @@ def upload_file(destiny: t.Tuple[str, str]) -> str:
     """
     directory, file_name = destiny
     source_path = f'{config.AWS_ASSETS_SOURCE}/{file_name}'
-    if ENV == 'production':
-        bucket = f'images-live-briefy'
-    else:
-        bucket = f'images-stg-briefy'
-    file_path = f'{directory}/{file_name}'
-
+    bucket = f'images-{_queue_suffix}-briefy'
+    file_path = os.path.join(directory, file_name)
     s3 = boto3.resource('s3')
     s3.meta.client.upload_file(file_path, bucket, source_path)
     logger.info(f'File name "{file_path}" uploaded to bucket "{bucket}"')
+    os.remove(file_path)
     return source_path
